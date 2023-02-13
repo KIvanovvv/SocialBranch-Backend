ca813c1eb96e09f65128304002b9f9a761edcfef
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-02-13 13:34:54 +0200

    CORS TEST

--- middlewares/CORS.js
+++ middlewares/CORS.js
@@ -1,15 +1,14 @@
 module.exports = () => (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://kivanovvv.github.io");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
 
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, X-Authorization"
-  );
+  res.setHeader("*");
 
   next();
 };
+// "Access-Control-Allow-Headers",
+// "Content-Type, X-Authorization"
